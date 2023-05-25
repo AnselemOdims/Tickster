e4f2d70582ff088d625f3bbe9f8f418237688e35
Anselem Odimegwu <odimegwu@systemspecs.com.ng>
2023-05-25 09:40:31 +0100

    feat: extend the signUp controller to include creating a new user and saving to the db

--- auth/src/controllers/signup.ts
+++ auth/src/controllers/signup.ts
@@ -1,20 +1,22 @@
 import { Request, Response } from 'express';
 import { validationResult } from 'express-validator';
-import { RequestValidationError, DatabaseConnectionError } from '../errors';
+import { RequestValidationError, BadRequest } from '../errors';
 import User from '../models/user'
 
-const signUp = (req: Request, res: Response) => {
+const signUp = async (req: Request, res: Response) => {
     const errors = validationResult(req)
     if(!errors.isEmpty()) {
         throw new RequestValidationError(errors.array())
     }
     const { name, email, password } = req.body
-    const existingUser = User.findOne({ email })
-    // if(existingUser) {
-
-    // }
-    // const user = User.build({ name, email, password })
-    res.send({ name, email, password })
+    const existingUser = await User.findOne({ email })
+    if(existingUser) {
+        throw new BadRequest('Email already in use')
+    }
+    
+    const user = User.build({ name, email, password })
+    await user.save()
+    res.status(201).send(user)
 }
 
 export default signUp
