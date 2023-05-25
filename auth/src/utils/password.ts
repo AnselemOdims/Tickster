e7412eb4326b0e108c4d3c382b068b2693001efe
Anselem Odimegwu <odimegwu@systemspecs.com.ng>
2023-05-25 11:12:38 +0100

    feat: create a Password manager class

---
+++ auth/src/utils/password.ts
@@ -0,0 +1,19 @@
+import { scrypt, randomBytes } from 'crypto';
+import { promisify } from 'util'
+
+const scryptAsync = promisify(scrypt)
+class Password {
+    static async hashPwd(pwd: string) {
+        const salt = randomBytes(16).toString('hex')
+        const buf = await scryptAsync(pwd, salt, 64) as Buffer;
+        return `${buf.toString('hex')}.${salt}}`
+    }
+
+    static async comparePwd(storedPwd: string, pwd: string) {
+        const [hashedPwd, salt] = storedPwd.split('.')
+        const newBuf = await scryptAsync(pwd, salt, 64) as Buffer;
+        return hashedPwd === newBuf.toString('hex')
+    }
+}
+
+export default Password;
