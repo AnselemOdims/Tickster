696e8e685c284cc18254265c85fa8f6b3e2a8569
Anselem Odimegwu <odimegwu@systemspecs.com.ng>
2023-06-04 04:02:45 +0100

    Add validation and request test for signup

--- auth/src/__test__/signup.test.ts
+++ auth/src/__test__/signup.test.ts
@@ -11,3 +11,62 @@ it('should return 201 on successful signup', () => {
             })
             .expect(201)
 })
+
+it('should return 400 with an invalid name', () => {
+    return request(app)
+            .post('/api/users/signup')
+            .send({
+                name: 'John',
+                email: 'johndoe@localhost.com',
+                password: 'password'
+            })
+            .expect(400)
+})
+
+it('should return 400 with an invalid email', () => {
+    return request(app)
+            .post('/api/users/signup')
+            .send({
+                name: 'John Doe',
+                email: 'johndoe@localhost',
+                password: 'password'
+            })
+            .expect(400)
+})
+
+it('should return 400 with an invalid password', () => {
+    return request(app)
+            .post('/api/users/signup')
+            .send({
+                name: 'John Doe',
+                email: 'johndoe@localhost.com',
+                password: 'pass'
+            })
+            .expect(400)
+})
+
+it('should return 400 with missing name, email and password', async () => {
+    await request(app)
+            .post('/api/users/signup')
+            .send({
+                email: 'johndoe@localhost.com',
+                password: 'password'
+            })
+            .expect(400)
+
+    await request(app)
+            .post('/api/users/signup')
+            .send({
+                name: 'John Doe',
+                password: 'password'
+            })
+            .expect(400)
+
+    await request(app)
+            .post('/api/users/signup')
+            .send({
+                name: 'John Doe',
+                email: 'johndoe@localhost.com'
+            })
+            .expect(400)
+})
