af0962516895681532d877a89063f470f3b47985
Anselem Odimegwu <odimegwu@systemspecs.com.ng>
2023-06-04 04:24:39 +0100

    add test for unregistered user trying to signinn

--- auth/src/__test__/signin.test.ts
+++ auth/src/__test__/signin.test.ts
@@ -2,18 +2,29 @@ import request from 'supertest';
 import { app } from '../app';
 
 it('should return 400 with missing email and password', async () => {
-    await request(app)
-        .post('/api/users/signin')
-        .send({
-            password: 'password'
-        })
-        .expect(400);
+  await request(app)
+    .post('/api/users/signin')
+    .send({
+      password: 'password',
+    })
+    .expect(400);
 
-    await request(app)
-        .post('/api/users/signin')
-        .send({
-            email: 'johndoe@localhost.com'
-        })
-        .expect(400);
-})
+  await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'johndoe@localhost.com',
+    })
+    .expect(400);
+});
+
+it('should return 400 for an unregistered user', async () => {
+  return request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'johndoe@localhost.com',
+      password: 'password',
+    })
+    .expect(400);
+});
+
 
