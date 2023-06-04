8f4a146082ae78a4b2b1886abbb0ed0cb2aeed14
Anselem Odimegwu <odimegwu@systemspecs.com.ng>
2023-06-04 04:29:57 +0100

    add test for signing in with correct credentials

--- auth/src/__test__/signin.test.ts
+++ auth/src/__test__/signin.test.ts
@@ -28,20 +28,41 @@ it('should return 400 for an unregistered user', async () => {
 });
 
 it('should return 400 for an invalid password', async () => {
-    await request(app)
-        .post('/api/users/signup')
-        .send({
-        name: 'John Doe',
-        email: 'johndoe@localhost.com',
-        password: 'password'
-        })
-        .expect(201);
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      name: 'John Doe',
+      email: 'johndoe@localhost.com',
+      password: 'password',
+    })
+    .expect(201);
 
-    await request(app)
-        .post('/api/users/signin')
-        .send({
-            email: 'johndoe@localhost.com',
-            password: 'pass'
-        })
-        .expect(400)
+  await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'johndoe@localhost.com',
+      password: 'pass',
+    })
+    .expect(400);
+});
+
+it('should return 200 for a valid user', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      name: 'John Doe',
+      email: 'johndoe@localhost.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  const res = await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'johndoe@localhost.com',
+      password: 'password',
+    })
+    .expect(200);
+
+    expect(res.get('Set-Cookie')).toBeDefined();
 })
