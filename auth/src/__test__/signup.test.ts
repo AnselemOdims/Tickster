cd7239be3d62e7a1b5d74843fd0ae0141f73b13b
Anselem Odimegwu <odimegwu@systemspecs.com.ng>
2023-06-04 04:05:45 +0100

    add tests to disallow dulicate emails

--- auth/src/__test__/signup.test.ts
+++ auth/src/__test__/signup.test.ts
@@ -1,72 +1,92 @@
-import request  from 'supertest';
+import request from 'supertest';
 import { app } from '../app';
 
 it('should return 201 on successful signup', () => {
-    return request(app)
-            .post('/api/users/signup')
-            .send({
-                name: 'John Doe',
-                email: 'johndoe@localhost.com',
-                password: 'password'
-            })
-            .expect(201)
-})
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      name: 'John Doe',
+      email: 'johndoe@localhost.com',
+      password: 'password',
+    })
+    .expect(201);
+});
 
 it('should return 400 with an invalid name', () => {
-    return request(app)
-            .post('/api/users/signup')
-            .send({
-                name: 'John',
-                email: 'johndoe@localhost.com',
-                password: 'password'
-            })
-            .expect(400)
-})
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      name: 'John',
+      email: 'johndoe@localhost.com',
+      password: 'password',
+    })
+    .expect(400);
+});
 
 it('should return 400 with an invalid email', () => {
-    return request(app)
-            .post('/api/users/signup')
-            .send({
-                name: 'John Doe',
-                email: 'johndoe@localhost',
-                password: 'password'
-            })
-            .expect(400)
-})
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      name: 'John Doe',
+      email: 'johndoe@localhost',
+      password: 'password',
+    })
+    .expect(400);
+});
 
 it('should return 400 with an invalid password', () => {
-    return request(app)
-            .post('/api/users/signup')
-            .send({
-                name: 'John Doe',
-                email: 'johndoe@localhost.com',
-                password: 'pass'
-            })
-            .expect(400)
-})
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      name: 'John Doe',
+      email: 'johndoe@localhost.com',
+      password: 'pass',
+    })
+    .expect(400);
+});
 
 it('should return 400 with missing name, email and password', async () => {
-    await request(app)
-            .post('/api/users/signup')
-            .send({
-                email: 'johndoe@localhost.com',
-                password: 'password'
-            })
-            .expect(400)
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'johndoe@localhost.com',
+      password: 'password',
+    })
+    .expect(400);
 
-    await request(app)
-            .post('/api/users/signup')
-            .send({
-                name: 'John Doe',
-                password: 'password'
-            })
-            .expect(400)
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      name: 'John Doe',
+      password: 'password',
+    })
+    .expect(400);
 
-    await request(app)
-            .post('/api/users/signup')
-            .send({
-                name: 'John Doe',
-                email: 'johndoe@localhost.com'
-            })
-            .expect(400)
-})
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      name: 'John Doe',
+      email: 'johndoe@localhost.com',
+    })
+    .expect(400);
+});
+
+it('should disallow duplicate emails', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      name: 'John Doe',
+      email: 'johndoe@localhost.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      name: 'John Doe',
+      email: 'johndoe@localhost.com',
+      password: 'password',
+    })
+    .expect(400);
+});
